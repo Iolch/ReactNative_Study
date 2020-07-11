12acac5e92ecdeaf9bfc25b9c1c7f987159f4eae
Iolanda Chagas <iccpaiva@gmail.com>
2020-07-11 00:07:48 -0300

    ADD: Complete functionality of adding element to list using useState

--- App.js
+++ App.js
@@ -1,26 +1,62 @@
 import React,{
-  useState 
-}from 'react';
+  useState
+} from 'react';
 import {
   Button,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from 'react-native';
 
 export default function App(){
-  const[ outputText, setOutputText ] = useState('Original Text');
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // This would also work 
+  // function goalInputHandler(inputText){setEnteredGoal(inputText);}
+  const goalInputHandler = (inputText) => {setEnteredGoal(inputText)};
+
+  // This would also work
+  // const addGoalHandler = () => { setCourseGoals([...courseGoals, enteredGoal])}; // Concatenation of the array course goals, with new one 
+  const addGoalHandler = () => { setCourseGoals(currentGoals => [...currentGoals, enteredGoal])}; // Guarantee we have the most updated courseGoals 
+  
   return(
       <View style={styles.container}>
-          <Text> {outputText} </Text>
-          <Button title='Click me' onPress={() => setOutputText('change')} />
+
+        <View style={styles.inputContainer}>
+          {/* Input VIEW */}
+
+          <TextInput 
+            style={styles.input} 
+            onChangeText={goalInputHandler} 
+            placeholder='Course Goal'
+            value = {enteredGoal}
+            ></TextInput>
+
+          <Button title='add' onPress={addGoalHandler}/>
+        </View>
+
+        <View>
+          {/* List VIEW */}
+          {courseGoals.map( (goal) => <Text key={goal} >{goal}</Text> )}
+        </View>
+
       </View>
   );
 }
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent:'center'
+    padding: 30
+  },
+  inputContainer:{
+    flexDirection:'row', // default as coloumn
+    justifyContent: 'space-between', // justify content horizontally
+    alignItems: 'center', // align items vertically
+  },
+  input:{ 
+    width: '80%', 
+    borderBottomColor: 'grey',
+    borderBottomWidth: 1
   },
 });
